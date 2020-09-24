365cbbc8a952303d71e5ab91db8695b79492db7f
X-Aleksey-X <TheHentWill@gmail.com>
2020-09-24 15:28:35 +0300

    Обработчик ошибок сделан теперь в виде файла, который импортируется в main.py

--- catnetbot/main.py
+++ catnetbot/main.py
@@ -1,11 +1,19 @@
-import discord
+from discord import utils
 from discord.ext import commands
 from loguru import logger
-import asyncio
 import toml_config
+import os
+import peewee
+import errors
+
+db = peewee.SqliteDatabase('catnet.db')
 
 config = toml_config.load_config()
-bot = commands.Bot(command_prefix=f'{config["bot"]["prefix"]}', case_insensitive=True)
+bot = commands.Bot(command_prefix = f'{config["bot"]["prefix"]}', case_insensitive = True)
+
+MINUTE = 60
+
+MUTE_ROLE_ID = config["discord"]["mute_role_id"]
 
 
 @bot.event
@@ -15,6 +23,18 @@ async def on_ready():
     """
     logger.info(f"Бот вошёл в сеть. Аккаунт: {bot.user}, ID аккаунта: {bot.user.id}")
 
+    @bot.event
+    async def on_command(ctx):
+        await ctx.message.delete()
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        await errors.command_error_detection(ctx, error)
 
 if __name__ == "__main__":  # если запускаем именно этот файл то входим в бота
+
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+
     bot.run(f'{config["discord"]["token"]}')
