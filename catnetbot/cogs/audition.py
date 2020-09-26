1555f24fea1e07a7ec595a0db3118d134063bf41
X-Aleksey-X <TheHentWill@gmail.com>
2020-09-26 19:37:31 +0300

    Аудирование удалённых сообщений

--- catnetbot/cogs/audition.py
+++ catnetbot/cogs/audition.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import discord
 from discord.ext import commands
 
@@ -18,12 +20,22 @@ ERROR_COLOR = conf["messages"]["errors"]["error_line"]["color"]
 
 INVISIBLE_SYMBOL = conf["messages"]["errors"]["invisible_symbol"]
 
+LOGS_CHANNEL_ID = conf["bot"]["logs_channel_id"]
 
 class Audition(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        emb = discord.Embed(color = ERROR_COLOR, timestamp = datetime.now())
+        emb.add_field(name = "Сообщение:", value = f"```{message.content[:1000]}```")
+        emb.add_field(name = f"{INVISIBLE_SYMBOL}", value = f"{ERROR_LINE}", inline = False)
+        emb.add_field(name = "ID сообщения:", value = f"{message.id}")
+        emb.add_field(name = "Автор сообщения:", value = f"{message.author}", inline = False)
+        emb.add_field(name = "Канал сообщения:", value = f"{message.channel}")
+        logs_channel = message.guild.get_channel(LOGS_CHANNEL_ID)
+        await logs_channel.send(embed = emb)
 
 def setup(bot):
     bot.add_cog(Audition(bot))
