e3b85e1100abbf1b31c215916d0bb9b1150339e2
X-Aleksey-X <TheHentWill@gmail.com>
2020-11-19 13:05:58 +0200

    Откорректировал под PEP8

--- catnetbot/toml_config.py
+++ catnetbot/toml_config.py
@@ -1,14 +1,16 @@
 """
 Небольшой модуль для управления конфигурацией.
-Нaписан давно (2 года назад) и все время мной использовался.
+Написан давно (2 года назад) и все время мной использовался.
 Является говнокодом, но работает
+- Merded
 """
 
 import os
-from shutil import copyfile
 import sys
+from shutil import copyfile
+
 import tomlkit
-from loguru import logger as l
+from loguru import logger
 
 
 FILE_NAME = "config.toml"
@@ -16,28 +18,37 @@ CONFIG_PATH = f"{FILE_NAME}"
 
 
 def copy_template():
-    """Копирует файл-шаблон"""
+    """
+    Копирует файл-шаблон
+    """
+
     try:
         copyfile("config_template.toml", CONFIG_PATH)
     except:
-        l.exception("Не удалось скопировать файл-шаблон.")
+        logger.exception("Не удалось скопировать файл-шаблон.")
         sys.exit(1)
 
 
 def config_find():
-    """Если конфиг не создан, создает"""
+    """
+    Если конфиг не создан, создает
+    """
+
     if not os.path.exists(CONFIG_PATH):
+
         copy_template()
         config_find()
-        l.warning("Конфигурация была создана.")
+
+        logger.warning("Конфигурация была создана.")
         sys.exit(0)
     if os.path.exists(CONFIG_PATH):
-        f = open(CONFIG_PATH, "r")
-    return f
+        with open(CONFIG_PATH, encoding = "utf-8") as file:
+            return file
 
 
 def load_config():
-    """Возвращает конфиг
+    """
+    Возвращает конфиг
     config.toml
     [test]
     abc = 123
@@ -48,6 +59,8 @@ def load_config():
     123
     """
 
-    with open(CONFIG_PATH, encoding = "utf-8") as f:
-        conf = tomlkit.parse(f.read())
-        return conf
+    config_find()
+
+    with open(CONFIG_PATH, encoding = "utf-8") as file:
+        config = tomlkit.parse(file.read())
+        return config
