3ff921b28c3186f88319244761ed1d2e0c79222f
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-12-21 00:27:22 +0200

    Update main.py

--- catnetbot/main.py
+++ catnetbot/main.py
@@ -1,46 +1,8 @@
-import os
-
-from discord.ext import commands
-from loguru import logger
-import peewee
-
-from catnetbot import toml_config
-from catnetbot import errors
+from bot import Bot
+from catnetbot import toml_config #import bot settings
 
 
-db = peewee.SqliteDatabase('catnet.db')
-
-config = toml_config.load_config()
-bot = commands.Bot(command_prefix = f'{config["bot"]["prefix"]}', case_insensitive = True, guild_subscriptions = True)
-
-MINUTE = 60
-
-
-@bot.event
-async def on_ready():
-    """
-    Информация о запуске бота
-    """
-    logger.info(f"Бот вошёл в сеть. Аккаунт: {bot.user}, ID аккаунта: {bot.user.id}")
-
-    @bot.event
-    async def on_command_completion(ctx):
-        await ctx.message.delete()
-
-    @bot.event
-    async def on_command_error(ctx, error):
-        await errors.command_error_detection(ctx, error)
-
-    bot.load_extension("jishaku")
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            bot.load_extension(f"cogs.{filename[:-3]}")
-
-if __name__ == "__main__":  # если запускаем именно этот файл то входим в бота
-
-    bot.run(f'{config["discord"]["token"]}')
-
-
-
-
-
+if __name__ == '__main__':
+    bot = Bot()
+    config = toml_config.load_config()
+    bot.run(prefix = config["bot"]["prefix"], token = config["discord"]["token"])
